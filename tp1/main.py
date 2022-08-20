739fc5cf97765a7f94e4b75f3882ac2fa968a806
IvanCsir <ivanfreiberg@gmail.com>
2022-08-20 17:46:30 -0300

    actualizado

--- tp1/main.py
+++ tp1/main.py
@@ -5,7 +5,7 @@ from random_search import *
 def main():
     n = int(input("How many rows will the matrix have?: "))
     m = int(input("How many columns will the matrix have?: "))
-    amount_mix = int(input("How many times doy you want to mix the elements?:" ))
+    amount_mix = int(input("How many times doy you want to mix the elements?: " ))
 
     objective_matrix = Matriz().crear_matriz(n,m)
     mixed_matrix = Matriz().mix_matrix(n,m,amount_mix)

b2d1e4914831b595b125e605431a23c3df486ef2
IvanCsir <ivanfreiberg@gmail.com>
2022-09-04 12:08:55 -0300

    anchura

--- tp1/busqueda_anchura.py
+++ tp1/busqueda_anchura.py
@@ -1,5 +1,41 @@
+from tree import TreeNode
 
 class Busqueda_anchura():
     
+    def find_zero(self,matrix):
+        
+        row1 = 0
+        col1 = 0
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == 0:
+                    row1 = row
+                    col1 = col
+                    return row1, col1
+
+    def possible_movements(self,matrix):
+        
+        row_0, col_0 = self.find_zero(matrix)
+
+        list_movements = []
+        if (row_0+1) < len(matrix):
+            list_movements.append(((row_0+1),col_0))
+
+        if (row_0-1) >= 0:
+            list_movements.append(((row_0-1),col_0))
+
+        if (col_0-1) >= 0:
+            list_movements.append((row_0,(col_0-1)))
+            
+        if (col_0+1) < len(matrix[0]):
+            list_movements.append((row_0,(col_0+1)))
+        
+        for i in len(list_movements):
+            movement = random.choice(list_movements)
+            row = movement[0]
+            col = movement[1]
+       
+        matrix[row_0][col_0], matrix[row][col] = matrix[row][col],  matrix[row_0][col_0]
+        return matrix
     def busqueda_anchura(self,matrix):
         pass
--- tp1/main_anchura.py
+++ tp1/main_anchura.py
@@ -1,5 +1,10 @@
+from matriz import *
+from busqueda_anchura import *
+
 def main():
     n = int(input("How many rows matrix do you want?: "))
     m = int(input("How many columns matrix do you want?: "))
     amount_mix = int(input("How many times doy you want to mix the elements?: "))
-    
+    matrix = Matriz().crear_matriz(n,m)
+    mixed_matrix = Matriz().mix_matrix(matrix) 
+    Busqueda_anchura().busqueda_anchura(mixed_matrix)

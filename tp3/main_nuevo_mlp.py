11355255f064629aaaff788f555e3be7455a88af
IvanCsir <ivanfreiberg@gmail.com>
2022-10-06 10:48:31 -0300

    multicapa

--- tp3/main_nuevo_mlp.py
+++ tp3/main_nuevo_mlp.py
@@ -1,7 +1,8 @@
 from nuevo_mlp import *
+import matplotlib.pyplot as plt
 
 def main():
-    LR = 0.1
+    LR = 0.5
     iteracion = 0
     xor_table = [[0, 0, 0],
                 [0, 1, 1],
@@ -20,74 +21,164 @@ def main():
     w10 = -0.79
     w11 = 0.56
     w12 = 0.6
-        
-    while iteracion < 1000:
-        iteracion += 1
-        print(f"Iteracion nro-------------------------------------------: {iteracion}")
-        for i in xor_table:
-            SR1 = Multi_layer_perceptron(i[0],i[1]).neuron_1(w0,w1,w2)
-            SR2 = Multi_layer_perceptron(i[0],i[1]).neuron_2(w3,w4,w5)
-            SR3 = Multi_layer_perceptron(i[0],i[1]).neuron_3(w6,w7,w8)
-            SR4, df = Multi_layer_perceptron(SR1,SR2).neuron_4(SR3,w9,w10,w11,w12)
+    w0_list = []
+    w1_list = []
+    w2_list = []
+    w3_list = []
+    w4_list = []
+    w5_list = []
+    w6_list = []
+    w7_list = []
+    w8_list = []
+    w9_list = []
+    w10_list = []
+    w11_list = []
+    w12_list = []
+    list_counter = []
+    list_error_counter = []
+    errors_r1 = []
+    errors_r2 = []
+    errors_r3 = []
+    errors_r4 = []    
+    
 
-            print("----Hidden Neuron 1 - Back forward----")
+    a = 0
+    
+    while iteracion < 10000:
+        iteracion += 1
+        print(f"---------------Iteracion nro {iteracion}------------------")
+        list_error_counter.append(iteracion)
+        r = 0
+        for i in xor_table:
+            a += 1
+            r += 1
+            list_counter.append(a)
+            
+            
+            SR1 = Multi_layer_perceptron(i[0],i[1]).neuron_1(w0,w1,w2, r)
+            SR2 = Multi_layer_perceptron(i[0],i[1]).neuron_2(w3,w4,w5,r)
+            SR3 = Multi_layer_perceptron(i[0],i[1]).neuron_3(w6,w7,w8,r)
+            SR4, df, w9, w10, w11,w12, error= Multi_layer_perceptron(SR1,SR2).neuron_4(i,SR3,w9,w10,w11,w12, r)
+
+            # print("----Hidden Neuron 1 - Back forward----")
             s_0c1 = SR1 * (1 - (SR1)) * df
             delta_w0_0c1 = LR * 1 * s_0c1
-            print(f"s_0c1:{s_0c1}")
-            print(f"delta_0c1:{delta_w0_0c1}")
-
+            # print(f"s_0c1:{s_0c1}")
+            # print(f"delta_0c1:{delta_w0_0c1}")
+            # print(f"w0: {w0}") 
             w0 = delta_w0_0c1 + w0
-            print(f"w0: {w0}")
+            
+            w0 = w0
+            w0_list.append(w0)
 
-            delta_w1_0c1 = LR * 0 * s_0c1
-            print(f"delta_w1_0c1:{delta_w1_0c1}")
+            delta_w1_0c1 = LR * i[0] * s_0c1
+            # print(f"delta_w1_0c1:{delta_w1_0c1}")
+            # print(f"w1: {w1}")
             w1 = delta_w1_0c1 + w1
-            print(f"w1: {w1}")
+            w1 = w1
+            w1_list.append(w1)
 
-            delta_w2_0c1 = LR * 0 * s_0c1
-            print(f"delta_w2_0c1:{delta_w2_0c1}")
+            delta_w2_0c1 = LR * i[1] * s_0c1
+            # print(f"delta_w2_0c1:{delta_w2_0c1}")
+            # print(f"w2: {w2}\n")
             w2 = delta_w2_0c1 + w2
-            print(f"w2: {w2}")
+            w2 = w2
+            w2_list.append(w2)
 
-            print("----Hidden Neuron 2 - Back forward----")
+            # print("----Hidden Neuron 2 - Back forward----")
             s_0c2 = SR2 * (1 - (SR2)) * df
             delta_w3_0c2 = LR * 1 * s_0c2
-            print(f"s_0c2:{s_0c2}")
-            print(f"delta_0c2:{delta_w3_0c2}")
-
+            # print(f"s_0c2:{s_0c2}")
+            # print(f"delta_0c2:{delta_w3_0c2}")
+            # print(f"w3: {w3}")
             w3 = delta_w3_0c2 + w3
-            print(f"w3: {w3}")
+            w3 = w3
+            w3_list.append(w3)
 
-            delta_w4_0c2 = LR * 0 * s_0c2
-            print(f"delta_w4_0c2:{delta_w4_0c2}")
+            delta_w4_0c2 = LR * i[0] * s_0c2
+            # print(f"delta_w4_0c2:{delta_w4_0c2}")
+            # print(f"w4: {w4}")
             w4 = delta_w4_0c2 + w4
-            print(f"w4: {w4}")
+            w4 = w4
+            w4_list.append(w4)
 
-            delta_w5_0c2 = LR * 0 * s_0c2
-            print(f"delta_w5_0c2:{delta_w5_0c2}")
+            delta_w5_0c2 = LR * i[1] * s_0c2
+            # print(f"delta_w5_0c2:{delta_w5_0c2}")
+            # print(f"w5: {w5}\n")
             w5 = delta_w5_0c2 + w5
-            print(f"w5: {w5}")
+            w5 = w5
+            w5_list.append(w5)
 
-            print("----Hidden Neuron 3 - Back forward----")
+            # print("----Hidden Neuron 3 - Back forward----")
             s_0c3 = SR3 * (1 - (SR3)) * df
             delta_w6_0c3 = LR * 1 * s_0c3
-            print(f"s_0c3:{s_0c3}")
-            print(f"delta_0c3:{delta_w6_0c3}")
-
+            # print(f"s_0c3:{s_0c3}")
+            # print(f"delta_0c3:{delta_w6_0c3}")
+            # print(f"w6: {w6}")
             w6 = delta_w6_0c3 + w6
-            print(f"w6: {w6}")
+            w6 = w6
+            w6_list.append(w6)
 
-            delta_w7_0c3 = LR * 0 * s_0c3
-            print(f"delta_w7_0c3:{delta_w7_0c3}")
+            delta_w7_0c3 = LR * i[0] * s_0c3
             w7 = delta_w7_0c3 + w7
-            print(f"w7: {w7}")
+            # print(f"delta_w7_0c3:{delta_w7_0c3}")
+            # print(f"w7: {w7}")
+            w7 = w7
+            w7_list.append(w7)
 
-            delta_w8_0c3 = LR * 0 * s_0c3
-            print(f"delta_w8_0c3:{delta_w8_0c3}")
+            delta_w8_0c3 = LR * i[1] * s_0c3
             w8 = delta_w8_0c3 + w8
-            print(f"w8: {w8}")
+            # print(f"delta_w8_0c3:{delta_w8_0c3}")
+            # print(f"w8: {w8}")
+            w8 = w8
+            w9 = w9
+            w10 = w10
+            w11 = w11
+            w12 = w12
+            w8_list.append(w8)
+            w9_list.append(w9)
+            w10_list.append(w10)
+            w11_list.append(w11)
+            w12_list.append(w12)
 
+            if i == xor_table[0]:
+                errors_r1.append(error)
+            elif i == xor_table[1]:
+                errors_r2.append(error)
+            elif i == xor_table[2]:
+                errors_r3.append(error)
+            elif i == xor_table[3]:
+                errors_r4.append(error) 
+        
+    plt.plot(list_counter, w0_list, label="W0")
+    plt.plot(list_counter, w1_list, label="W1")
+    plt.plot(list_counter, w2_list, label="W2")
+    plt.plot(list_counter, w3_list, label="W3")
+    plt.plot(list_counter, w4_list, label="W4")
+    plt.plot(list_counter, w5_list, label="W5")
+    plt.plot(list_counter, w6_list, label="W6")
+    plt.plot(list_counter, w7_list, label="W7")
+    plt.plot(list_counter, w8_list, label="W8")
+    plt.plot(list_counter, w9_list, label="W9")
+    plt.plot(list_counter, w10_list, label="W10")
+    plt.plot(list_counter, w11_list, label="W11")
+    plt.plot(list_counter, w12_list, label="W12")
+    plt.xlabel("Iteracion")
+    plt.ylabel("Peso")
+    plt.title("PESOS")
+    plt.legend()
+    plt.show()
 
+    plt.plot(list_error_counter, errors_r1, label = "Error r1")
+    plt.plot(list_error_counter, errors_r2, label = "Error r2")
+    plt.plot(list_error_counter, errors_r3, label = "Error r3")
+    plt.plot(list_error_counter, errors_r4, label = "Error r4")
+    plt.xlabel("Iteracion")
+    plt.ylabel("Error")
+
+    plt.title("ERRORES")
+    plt.legend()
+    plt.show()
 
 
 
--- tp3/nuevo_mlp.py
+++ tp3/nuevo_mlp.py
@@ -6,67 +6,26 @@ class Multi_layer_perceptron():
     def __init__(self,e1, e2):
         self.e1 = e1
         self.e2 = e2
-        self.LR = 0.1
-        self.xor_table = [[0, 0, 0],
-                         [0, 1, 1],
-                         [1, 0, 1],
-                         [1, 1, 0]]
-    def neuron_1(self,w0, w1, w2):
-        
-        w0 = 0.9
-        w1 = 0.7
-        w2 = 0.5
+        self.LR = 0.5
 
-
-        self.e1 = self.xor_table[0][0]
-        self.e2 = self.xor_table[0][1]
+    def neuron_1(self,w0, w1, w2, iteracion):
 
         x = (1*w0 + self.e1*w1 + self.e2*w2)
         SR = 1/(1+(math.e)**(-x))
-        # error = self.xor_table[0][2] - SR
-        # epsilon = SR*(1-(SR))*error
-
-        # delta_w0 = self.LR*1*epsilon
-        # w0 = w0 + delta_w0
-
-        # delta_w1 = self.LR*self.e1*epsilon
-        # w1 = w1 + delta_w1
-
-        # delta_w2 = self.LR*self.e2*epsilon
-        # w2 = w2 + delta_w2
+        print(f"---Neuron 1 Iteracion:{iteracion}---")
+        print(f"SR: {SR}\n")
         
-        print("---Neuron 1---\n")
-        # print(f"W0: {w0}")
-        # print(f"W1: {w1}")
-        # print(f"W2: {w2}")
-        print(f"SR: {SR}")
-        # print(f"Error: {error} \n")
-        return SR, w0, w1, w2
+        return SR
 
-    def neuron_2(self, w3, w4, w5):
+    def neuron_2(self, w3, w4, w5, iteracion):
         
         x = (1*w3 + self.e1*w4 + self.e2*w5)
         SR = 1/(1+(math.e)**(-x))
-        # error = self.xor_table[0][2] - SR
-        # epsilon = SR*(1-(SR))*error
-
-        # delta_w3 = self.LR*1*epsilon
-        # w3 = w3 + delta_w3
-
-        # delta_w4 = self.LR*self.e1*epsilon
-        # w4 = w4 + delta_w4
-
-        # delta_w5 = self.LR*self.e2*epsilon
-        # w5 = w5 + delta_w5
-        print("---Neuron 2---\n")
-        # print(f"W3: {w3}")
-        # print(f"W4: {w4}")
-        # print(f"W5: {w5}")
-        print(f"SR: {SR}")
-        # print(f"Error: {error} \n")
+        print(f"---Neuron 2 Iteracion:{iteracion}---")
+        print(f"SR: {SR}\n")
         return SR
     
-    def neuron_3(self, w6, w7, w8):
+    def neuron_3(self, w6, w7, w8,iteracion):
 
         x = (1*w6 + self.e1*w7 + self.e2*w8)
         SR = 1/(1+(math.e)**(-x))
@@ -82,20 +41,20 @@ class Multi_layer_perceptron():
         # delta_w8 = self.LR*self.e2*epsilon
         # w8 = w8 + delta_w8
 
-        print("---Neuron 3---\n")
+        print(f"---Neuron 3 Iteracion:{iteracion}---")
         # print(f"W6: {w6}")
         # print(f"W7: {w7}")
         # print(f"W8: {w8}")
-        print(f"SR: {SR}")
+        print(f"SR: {SR}\n")
         # print(f"Error: {error} \n")
         return SR
     
-    def neuron_4(self,e3, w9,w10,w11,w12):
+    def neuron_4(self,row,e3,w9,w10,w11,w12, iteracion):
         self.e3 = e3
-        x = (float(1*w9) + self.e1*w10 + self.e2*w11 + self.e3*w12)
+        x = (1*w9 + self.e1*w10 + self.e2*w11 + self.e3*w12)
         
         SR = 1/(1+(math.e)**(-x))
-        error = self.xor_table[0][2] - SR
+        error = row[2] - SR
         epsilon = SR*(1-(SR))*error
 
         delta_w9 = self.LR*1*epsilon
@@ -110,7 +69,7 @@ class Multi_layer_perceptron():
         delta_w12 = self.LR*self.e3*epsilon
         w12 = w12 + delta_w12
 
-        print("---Neuron 4---\n")
+        print(f"---Neuron 4 Iteracion:{iteracion}---")
         print(f"w9: {w9}")
         print(f"w10: {w10}")
         print(f"W11: {w11}")
@@ -119,4 +78,4 @@ class Multi_layer_perceptron():
         print(f"x = {x}")
         print(f"Error: {error} \n")
 
-        return SR, epsilon
+        return SR, epsilon, w9, w10, w11,w12, error
